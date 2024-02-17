72d65b83bb385d5dbbbad26023bf9a6a82e28922
dev-coder <muzammalikram781@gmail.com>
2024-02-17 23:10:17 +0300

    ..

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint"],
     root: true,
-    ignorePatterns: ["public/", "build/", "src/ui"],
+    ignorePatterns: ["public/", "build/", "src"],
     rules: {
         "@typescript-eslint/no-unused-vars": "error",
         "react/no-unused-prop-types": 2
